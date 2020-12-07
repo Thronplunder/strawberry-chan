1e0d1edcba98b576cd11cfeaa7bc00281dc46646
Alessandro Kiefer <alessandrokiefer@gmail.com>
2020-12-08 00:06:43 +0100

    first output

--- strawberrychan.py
+++ strawberrychan.py
@@ -1,3 +1,21 @@
+import discord
 token = open('token.txt', 'r').read()
 
 print(token)
+
+strawberrychan = discord.Client()
+
+
+@strawberrychan.event
+async def on_ready():
+    print("logged in as {0.user}", strawberrychan)
+
+
+@strawberrychan.event
+async def on_message(message):
+    if message.author == strawberrychan.user:
+        return
+    else:
+        print("{0.author} send the following: {0.content}", message)
+
+strawberrychan.run(token)
