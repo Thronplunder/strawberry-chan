2b7e9eb37e5917d597e29a7dbaa823b93d2f10b9
Alessandro Kiefer <alessandrokiefer@gmail.com>
2020-12-10 00:13:11 +0100

    first queue implementation started

---
+++ queue.py
@@ -0,0 +1,12 @@
+class Queue:
+    def __init__(self):
+        self.players = []
+
+        def addplayer(self, playerid):
+            self.players.append(playerid)
+
+        def emptyqueue():
+            del self.players[:]
+
+        def getqueuelength(self):
+            return len(self.players)
